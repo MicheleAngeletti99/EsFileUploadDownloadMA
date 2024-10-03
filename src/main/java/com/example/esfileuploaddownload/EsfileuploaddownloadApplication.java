aa967a242d9fc6cd1bbf453c3d1ec0083d230825
Michele Angeletti <m.angeletti99@gmail.com>
2024-10-03 14:30:19 +0200

    Esercizio

---
+++ src/main/java/com/example/esfileuploaddownload/EsfileuploaddownloadApplication.java
@@ -0,0 +1,13 @@
+package com.example.esfileuploaddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsfileuploaddownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsfileuploaddownloadApplication.class, args);
+	}
+
+}
